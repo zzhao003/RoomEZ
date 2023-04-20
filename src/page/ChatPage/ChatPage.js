0eed6d68bbcc86fbd6ca40a8e6e191ab3cb824e3
zzhao003 <zzhao003@gmail.com>
2023-04-20 16:40:10 -0400

    adds useEffect dependencies for chatpage

--- src/page/ChatPage/ChatPage.js
+++ src/page/ChatPage/ChatPage.js
@@ -27,7 +27,7 @@ const ChatPage = () => {
         id: user.id,
       })
       .then((res) => {
-        console.log(res.data);
+        // console.log(res.data);
 
         const filteredMatch = res.data;
         // .map((item) => {
@@ -41,7 +41,7 @@ const ChatPage = () => {
         setMatchArr(filteredMatch);
       })
       .catch((err) => console.log(err));
-  }, []);
+  }, [navigate, user]);
 
   return (
     <div className="chatpage">
